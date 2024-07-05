96b436b7d272d4610c03df7cfd5dd2de5b042efc
anas-ma007 <anaschelakkara786@gmail.com>
2024-07-05 18:56:30 +0530

    Add register form

--- client-social-chat/src/pages/RegisterPage.jsx
+++ client-social-chat/src/pages/RegisterPage.jsx
@@ -1,8 +1,23 @@
 import React, { useState } from 'react'
 
 const RegisterPage = () => {
-  // const [data, setData] = useState([])
-  // console.log(data, "Data log");
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    profile_pic: ""
+  })
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value
+      }
+    })
+  }
+  console.log(data, "Data log");
   return (
 
     <div className="p-2 pt-0">
@@ -10,16 +25,50 @@ const RegisterPage = () => {
         <p className='text-xl text-slate-800 font-extrabold font-serif'>Welcome to Social Chat..!</p>
 
         <form action="">
-          <div className='py-2 '>
+          <div className='py-2 flex flex-col gap-2'>
             <label className='font-semibold' htmlFor="name"> Name : </label>
             <input
+              className='bg-slate-200 px-5 py-1 rounded shadow-2xl focus:bg-slate-300 focus:outline-primary'
               type='text'
               id='name'
               name='name'
               placeholder='Enter your name'
-              className='bg-slate-200 px-5 py-1 rounded shadow-2xl focus:bg-slate-300 focus:outline-primary'
+              value={data.name}
+              onChange={handleOnChange}
+              required
             />
           </div>
+
+          <div className='py-2 flex flex-col gap-2'>
+            <label className='font-semibold' htmlFor="email"> Email : </label>
+            <input
+              className='bg-slate-200 px-5 py-1 rounded shadow-2xl focus:bg-slate-300 focus:outline-primary'
+              type='email'
+              id='email'
+              name='email'
+              placeholder='Enter your email'
+              value={data.email}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+
+
+          <div className='py-2 flex flex-col gap-2'>
+            <label className='font-semibold' htmlFor="password"> Password : </label>
+            <input
+              className='bg-slate-200 px-5 py-1 rounded shadow-2xl focus:bg-slate-300 focus:outline-primary'
+              type='password'
+              id='password'
+              name='password'
+              placeholder='Enter your password'
+              value={data.password}
+              onChange={handleOnChange}
+              required
+            />
+          </div>
+
+
         </form>
       </div>
 
