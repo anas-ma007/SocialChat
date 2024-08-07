e1478312c3bbf713b28236e50ea3327640eda0b1
anas-ma007 <anaschelakkara786@gmail.com>
2024-08-07 21:32:38 +0530

    change background color

--- client-social-chat/src/helpers/uploadFile.jsx
+++ client-social-chat/src/helpers/uploadFile.jsx
@@ -3,7 +3,6 @@ import { CLOUDINARY_CLOUD_NAME } from "../utils/constants";
 
 
 const url = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/auto/upload`  //I think here the "auto" is for all kind of data or media, means photos images..etc. not sure, need to check once again
-console.log(url, "url log");
 
 const uploadFile = async (file) => {
     const formData = new FormData()
