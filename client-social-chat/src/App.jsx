3c2f87972ae4db1273ae8d4dde5c17769ef4c8f9
anas-ma007 <anaschelakkara786@gmail.com>
2024-08-13 22:53:16 +0530

    app

--- client-social-chat/src/App.jsx
+++ client-social-chat/src/App.jsx
@@ -3,17 +3,6 @@ import toast, { Toaster } from 'react-hot-toast';
 import { useEffect } from 'react';
 
 const App = () => {
-  // const navigate = useNavigate()
-  // const location = useLocation()
-
-  // const emailIdUserData = location?.state?.data || {}
-  // useEffect(() => {
-  //   if (!emailIdUserData?.name) {
-  //     navigate("/email")
-  //   }
-  // }, [])
-
-
   return (
     <>
       <Toaster />
